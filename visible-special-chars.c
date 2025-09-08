25f2f3c7a4bfe7b761ca2dcae0822e30186cf113
Pedro Matoso <pedroomatoso@gmail.com>
2025-09-08 01:39:58 -0300

    feat: add visible special characters program

---
+++ visible-special-chars.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// Write a program to copy its input to its output, replacing each tab by \t, each backspace by \b, and each backslash by \\. This makes tabs and backspaces visible in an unambiguous way
+int main()
+{
+    int c;
+
+    while ((c = getchar()) != EOF) {
+        if (c == '\t') {
+            printf("\\t");
+            continue;
+        }
+        if (c == '\b') {
+            printf("\\b");
+            continue;
+        }
+        if (c == '\\') {
+            printf("\\\\");
+            continue;
+        }
+        putchar(c);
+    }
+    return 0;
+}
